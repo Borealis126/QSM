c11f94b0c1b8d034707b8fa54e9041b607a24e57
68707 <68707@NIST.GOV>
2021-05-21 23:46:39 -0600

    typo

--- Example/TwoQubit/driver.py
+++ Example/TwoQubit/driver.py
@@ -1,7 +1,7 @@
 import sys
 from pathlib import Path
 computeLocation = "Windows"  # Edit this based on where the QSM is being run. Should be "Windows" for most users.
-QSMSourceFolder = Path("/beegfs/scratch/joelhoward/QSMSimulations/QSMSource/src")
+QSMSourceFolder = Path(r"O:\68707\JoelHoward\ChipDesign\QSMSource_v2\QSM\src")
 sys.path.append(str(QSMSourceFolder))
 import qubitSimulationModule as QSM
 from simulations import *
@@ -29,7 +29,7 @@ projectFolder = Path(__file__).parent.absolute()
 # qSys = QSM.initialize(projectFolder, computeLocation, QSMSourceFolder, layoutCompleted=True)
 # qSys.generateGDS(addMesh=False)
 
-"""Generate the HFSS Model"""
+"""Generate the HFSS model"""
 # qSys = QSM.initialize(projectFolder, computeLocation, QSMSourceFolder, layoutCompleted=True)
 # HFSSModel(qSys).initialize()
 # HFSSModel(qSys).run()
@@ -39,45 +39,45 @@ projectFolder = Path(__file__).parent.absolute()
 needs to be run before every command. Once geometries are completed (i.e., now) switch layoutCompleted to True"""
 
 """Now uncomment this and leave it uncommented."""
-# qSys = QSM.initialize(projectFolder, computeLocation, QSMSourceFolder, layoutCompleted=True)
+qSys = QSM.initialize(projectFolder, computeLocation, QSMSourceFolder, layoutCompleted=True)
 
 """Run the following analyses"""
 
 # CapMat(qSys).initialize() # -> Normally populate, but use the default values for now.
 # CapMat(qSys).run()
 # CapMat(qSys).postProcess()
-#
-# for readoutResonatorIndex, readoutResonator in qSys.allReadoutResonatorsDict.items():
-#     LumpedR(readoutResonatorIndex)(qSys).initialize() # -> Populate the simParams inside the LumpedR folder.
-#
-# for readoutResonatorIndex, readoutResonator in qSys.allReadoutResonatorsDict.items():
-#     LumpedR(readoutResonatorIndex)(qSys).run()
 
-"""From here until Quantize everything can be run at once (uncomment all lines through 67) since there is no
+# for readoutResonatorIndex, readoutResonator in qSys.allReadoutResonatorsDict.items():
+    # LumpedR(readoutResonatorIndex)(qSys).initialize() # -> Populate the simParams inside the LumpedR folder.
+
+# for readoutResonatorIndex, readoutResonator in qSys.allReadoutResonatorsDict.items():
+    # LumpedR(readoutResonatorIndex)(qSys).run()
+
+"""From here through Quantize initialize everything can be run at once since there is no
 json populating or ansys simulations."""
 # for readoutResonatorIndex, readoutResonator in qSys.allReadoutResonatorsDict.items():
-#     LumpedR(readoutResonatorIndex)(qSys).postProcess()
-#
+    # LumpedR(readoutResonatorIndex)(qSys).postProcess()
+
 # CapMatGE(qSys).initialize()
 # CapMatGE(qSys).postProcess()
-#
+
 # for qubitIndex, qubit in qSys.allQubitsDict.items():
-#     ECQ(qubitIndex)(qSys).initialize()  # Only creates directory, no simParams file.
-#     ECQ(qubitIndex)(qSys).postProcess()
-#
+    # ECQ(qubitIndex)(qSys).initialize()  # Only creates directory, no simParams file.
+    # ECQ(qubitIndex)(qSys).postProcess()
+
 # for readoutResonatorIndex,readoutResonator in qSys.allReadoutResonatorsDict.items():
-#     ECR(readoutResonatorIndex)(qSys).initialize()
-#     ECR(readoutResonatorIndex)(qSys).postProcess()
-#
+    # ECR(readoutResonatorIndex)(qSys).initialize()
+    # ECR(readoutResonatorIndex)(qSys).postProcess()
+
 # Quantize(qSys).initialize() # -> Populate
 # Quantize(qSys).postProcess()
-#
+
 # print(ZZQ(qSys, 0, 1))
-#
+
 # print(L_iQ(qSys, 0))
-#
+
 # print(anharmonicityQ(qSys, 0))
-#
+
 # print(dispersiveShiftR(qSys, 0))
 
 """Congrats! You have simulated a 2-qubit system. If you don't like any calculated values, you can go back and
