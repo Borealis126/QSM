893daffec5811cd805755acb48e16b52c30d8e5e
Joel Howard <jhoward@rigetti.com>
2022-11-20 12:43:28 -0800

    BBQ now saving hamiltonian

--- src/BBQ.py
+++ src/BBQ.py
@@ -7,10 +7,10 @@ import numpy as np
 from .ansysAPI import ansysOutputToComplex
 from scipy.interpolate import interp1d
 from scipy.optimize import fsolve
-from .dataIO import jsonWrite
 import qutip as qt
 from .constants import hbarConst, GHzToOmega, Joules_To_GHz, Phi_0Const
 from scipy.misc import derivative
+
 
 class BBQ(Simulation):
     def __init__(self, qArch):
@@ -29,7 +29,7 @@ class BBQ(Simulation):
             quantizeList += resonator.name + ":"
         quantizeList = quantizeList[0:-1] + "]"
         simParamsDict.update({"QuantizeList": quantizeList, "NumResonatorPhotons": 5, "NumQubitPhotons": 5,
-                         "TrigOrder": 10})
+                              "TrigOrder": 10})
         self.generateParams(simParamsDict)
 
     def run(self):
@@ -42,50 +42,44 @@ class BBQ(Simulation):
         omegas = df['Freq [GHz]'].astype(float) * GHzToOmega
         num_qubits = len(self.qArch.allQubitsDict)
         num_resonators = len(self.qArch.allReadoutResonatorsDict)
-        num_components = num_qubits+num_resonators
+        num_components = num_qubits + num_resonators
         Y = np.empty((len(omegas), num_components, num_components), dtype=complex)
         for w in range(len(omegas)):
             for i in range(num_components):
                 for j in range(num_components):
-                    Y[w, i, j] = df['Y('+str(i+1)+','+str(j+1)+') [mSie]'][w] * 1e-3 #Convert to Sie
+                    Y[w, i, j] = df['Y(' + str(i + 1) + ',' + str(j + 1) + ') [mSie]'][w] * 1e-3  # Convert to Sie
         Z = np.array([np.linalg.inv(Y[w, :, :]) for w in range(len(omegas))])
 
-        Y_interp = np.array([[interp1d(omegas, Y[:, i, j]) for i in range(num_components)] for j in range(num_components)])
-        Z_interp = np.array([[interp1d(omegas, Z[:, i, j]) for i in range(num_components)] for j in range(num_components)])
+        Y_interp = np.array(
+            [[interp1d(omegas, Y[:, i, j]) for i in range(num_components)] for j in range(num_components)])
+        Z_interp = np.array(
+            [[interp1d(omegas, Z[:, i, j]) for i in range(num_components)] for j in range(num_components)])
 
-        N=5
+        N = 5
         k = 1
         omega_ps = [fsolve(lambda x: np.imag(Y_interp[i, i](x)), omegas[0])[0] for i in range(num_components)]
         a_s = [qt.tensor([qt.qeye(N)] * i + [qt.destroy(N)] + [qt.qeye(N)] * (num_components - i - 1))
                for i in range(num_components)]
         Z_kp_effs = [2 / (omega_ps[p] * np.imag(derivative(Y_interp[k, k], omega_ps[p], omegas[1] - omegas[0])))
                      for p in range(num_components)]
-        phi_ells = [sum([Z_interp[l, k](omega_ps[p]) / Z_interp[k, k](omega_ps[p]) * np.sqrt(1/2*Z_kp_effs[p])*(a_s[p]+a_s[p].dag())
-                        for p in range(num_components)])
+        phi_ells = [sum([Z_interp[l, k](omega_ps[p]) / Z_interp[k, k](omega_ps[p]) * np.sqrt(1 / 2 * Z_kp_effs[p]) * (
+                    a_s[p] + a_s[p].dag())
+                         for p in range(num_components)])
                     for l in range(num_components)]
 
-        H_0 = sum([omega_ps[p]*Joules_To_GHz*a_s[p].dag()*a_s[p] for p in range(num_components)])*hbarConst
-        H_nl = sum([-phi_ells[i]**4/(24*Phi_0Const**2*self.qArch.allQubitsDict[i].LJ) for i in range(num_qubits)])*Joules_To_GHz*hbarConst**2
+        H_0 = sum([omega_ps[p] * Joules_To_GHz * a_s[p].dag() * a_s[p] for p in range(num_components)]) * hbarConst
+        H_nl = sum([-phi_ells[i] ** 4 / (24 * Phi_0Const ** 2 * self.qArch.allQubitsDict[i].LJ) for i in
+                    range(num_qubits)]) * Joules_To_GHz * hbarConst ** 2
         H = H_0 + H_nl
 
         print(H_0.eigenenergies()[0:10])
 
         eigs = H.eigenenergies()
-        eigs_zeroed = eigs-eigs[0]
+        eigs_zeroed = np.real(eigs - eigs[0])
         print(eigs_zeroed[0:10])
+        qt.qsave(H, self.directoryPath / 'hamiltonian')
+        np.savetxt(self.directoryPath / "eigenvalues.csv", eigs_zeroed, delimiter=",")
 
-        #     freq_data = df['Freq [GHz]']
-        #     y_data = np.imag(df['Y('+str(qubitIndex+1)+','+str(qubitIndex+1)+') [mSie]'].apply(ansysOutputToComplex).astype(complex))
-        #     ax.plot(freq_data, y_data)
-        #     resultsDict['Q'+str(qubitIndex)+"Freq (GHz)"] = fsolve(interp1d(freq_data, y_data), freq_data[0])[0]
-        # N = len(self.qArch.allQubitsDict)
-        # for resonatorIndex, resonator in self.qArch.allReadoutResonatorsDict.items():
-        #     freq_data = df['Freq [GHz]']
-        #     y_data = np.imag(df['Y('+str(resonatorIndex+N+1)+','+str(resonatorIndex+N+1)+') [mSie]'].apply(ansysOutputToComplex).astype(complex))
-        #     ax.plot(freq_data, y_data)
-        #     resultsDict['R'+str(resonatorIndex)+"Freq (GHz)"] = fsolve(interp1d(freq_data, y_data), freq_data[0])[0]
-        # plt.savefig(self.directoryPath / 'Yplots.png')
-        # jsonWrite(self.resultsFilePath, resultsDict)
 
 class Y_BBQ_Simulation(CircuitSim):
 
@@ -131,9 +125,9 @@ class Y_BBQ_Simulation(CircuitSim):
             node_2 = self.netlistName(resonator.design.pad2.node.name)
             portsLines += [
                 "RPort" + str(resonatorIndex + N) + " " + node_1 + " " + node_2 + " PORTNUM=" + str(
-                    resonatorIndex+N) + " RZ=50\n",
-                ".PORT " + node_1 + " " + node_2 + " " + str(resonatorIndex+N) + " RPort" + str(
-                    resonatorIndex+N) + "\n"]
+                    resonatorIndex + N) + " RZ=50\n",
+                ".PORT " + node_1 + " " + node_2 + " " + str(resonatorIndex + N) + " RPort" + str(
+                    resonatorIndex + N) + "\n"]
         return portsLines
 
     @staticmethod
@@ -147,7 +141,7 @@ class Y_BBQ_Simulation(CircuitSim):
 
     @property
     def reportLines(self):
-        N=len(self.qArch.allQubitsDict) + len(self.qArch.allReadoutResonatorsDict)
+        N = len(self.qArch.allQubitsDict) + len(self.qArch.allReadoutResonatorsDict)
         return [
             "oModuleReport.CreateReport(\"Y Parameter Table 1\", \"Standard\", \"Data Table\", \"LNA\",\n",
             "    [\n",
@@ -159,8 +153,8 @@ class Y_BBQ_Simulation(CircuitSim):
             "    ],\n",
             "    [\n",
             "        \"X Component:=\"		, \"F\",\n",
-            "        \"Y Component:=\"		, ["+    ",".join(["\"Y("+str(i[0]+1)+","+str(i[1]+1)+")\""
-                                                                 for i in product(range(N),range(N))])  +"]\n",
+            "        \"Y Component:=\"		, [" + ",".join(["\"Y(" + str(i[0] + 1) + "," + str(i[1] + 1) + ")\""
+                                                               for i in product(range(N), range(N))]) + "]\n",
             "    ], [])\n",
             (
                     "oModuleReport.ExportToFile(\"Y Parameter Table 1\", r\""
